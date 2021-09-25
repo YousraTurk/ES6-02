588d271d9092b83571cfbb4a608dfb66221d8b55
YousraTurk <yousraarif1354@gmail.com>
2021-09-26 10:42:39 +1300

    ES6#02

---
+++ app.js
@@ -0,0 +1,67 @@
+//***************************Question#01***************************
+
+//write a program which shows the couter after each second.
+
+// var count = 0;
+// counter = () => {
+//     console.log(count++)
+// }
+// var abc=setInterval(counter,1000)
+// console.log(abc)
+
+//***************************Question#02***************************
+
+// write a function which takes your name and displays the greeting with your name
+
+// let firstName = prompt(" Hey user Enter your First Name ....... ")
+// let LstName = prompt(" Hey user Enter your Last Name ....... ")
+// greetUser = () => {
+//     console.log(`Hey! ${firstName} ${LstName} Welcome to My World ..) `)
+// }
+// greetUser()
+
+//***************************Question#03***************************
+
+// write a function(arrow function) which takes two values and return its sum as a result
+// let num1 = +prompt(" Hey user Enter a Number1  ....... ")
+// let num2 = +prompt(" Hey user Enter a Number2  ....... ")
+
+// sum = (a,b) => a+b;
+// console.log(`sum is  ${sum(num1,num2)}`)
+
+//***************************Question#04***************************
+
+
+//write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+
+// let num = +prompt(" Hey user Enter a Number  ....... ")
+
+// multiplication = () => num*0.5 
+// let a = multiplication()
+// console.log(`Your answer is ${a}`)
+
+
+//***************************Question#05***************************
+
+//print simple array of [1,2,3,4,5] with the help of array map funtion
+
+// const arry = [1,2,3,4,5]
+// let a = arry.map((i)=> i)
+// console.log(a)
+
+//***************************Question#06***************************
+
+// let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}]
+// iterate the given array through map function and print the name and id
+
+// const arr = [
+//     {id:1,name:"abc"},
+//     {id:1,name:"efg"},
+//     {id:2,name:"hij"},
+//     {id:3,name:"xyz"}
+// ]
+
+// let a = arr.map((i)=>{
+// console.log(`${i.id} ${i.name}`)
+// })
+
